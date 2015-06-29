809d75c1bd5bb208941e5d46fb7172fdc2081403
Taylor Premo <tain.premo@gmail.com>
2015-06-29 02:33:03 -0500

    Fixed some css issues & started on the game selection scree.

--- app/app.config.js
+++ app/app.config.js
@@ -1,54 +1,46 @@
- stupidStratGenApp.config(function($stateProvider, $urlRouterProvider) {
-     $urlRouterProvider
-         .otherwise('/404');
+stupidStratGenApp.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider
+        .otherwise('/404');
 
-     $stateProvider
-         .state('root', {
-             url: '',
+    $stateProvider
+        .state('root', {
+            url: '',
 
-             views: {
-                 "headerView": {
-                     templateUrl: 'app/shared/header/header.html'
-                 },
-                 "navView": {
-                     templateUrl: 'app/shared/nav/nav.html'
-                 },
-                 "mainView": {
-                     templateUrl: 'app/components/home/home.html'
-                 },
-                 "footerView": {
-                     templateUrl: 'app/shared/footer/footer.html'
-                 }
-             }
-         })
-         .state('root.home', {
-             url: '/',
+            views: {
+                "headerView": {
+                    templateUrl: 'app/shared/header/header.html'
+                },
+                "navView": {
+                    templateUrl: 'app/shared/nav/nav.html'
+                },
+                "mainView": {
+                    templateUrl: 'app/components/home/home.html'
+                },
+                "footerView": {
+                    templateUrl: 'app/shared/footer/footer.html'
+                }
+            }
+        })
+        .state('root.home', {
+            url: '/',
 
-             views: {
-                 "headerView@": {
-                     controller: 'homeController',
-                     templateUrl: 'app/shared/header/header.html'
-                 },
-                 "mainView@": {
-                     controller: 'homeController',
-                     templateUrl: 'app/components/home/home.html'
-                 }
-             }
-         })
-         .state('root.game', {
-             //this may need to be for each game.
-             //or maybe post or something can handle it
-             url: '/game',
+            views: {
+                "mainView@": {
+                    controller: 'homeController',
+                    templateUrl: 'app/components/home/home.html'
+                }
+            }
+        })
+        .state('root.game', {
+            //this may need to be for each game.
+            //or maybe post or something can handle it
+            url: '/game',
 
-             views: {
-                 "headerView@": {
-                     controller: 'gameController',
-                     templateUrl: 'app/shared/header/header.html'
-                 },
-                 "mainView@": {
-                     controller: 'gameController',
-                     templateUrl: 'app/components/game/game.html'
-                 }
-             }
-         });
- });
+            views: {
+                "mainView@": {
+                    controller: 'gameChoiceController',
+                    templateUrl: 'app/components/game/gameChoiceScreen.html'
+                }
+            }
+        });
+});
---
+++ app/components/Game/gameChoiceController.js
@@ -0,0 +1,42 @@
+stupidStratGenApp.controller('gameListController', function($scope) {
+    $scope.games = [{
+        'title': 'Team Fortress 2',
+        'description': 'team fortress 2 is fun',
+        'link': '#TF2',
+        'categories': [{
+            'title': 'Full Team',
+            'link': '#TF2/fullTeam',
+            'image': './assets/img/tf2FullTeam.jpg'
+        }, {
+            'title': 'Party',
+            'link': '#TF2/party',
+            'image': './assets/img/tf2Party.jpg'
+        }, {
+            'title': 'Two Players',
+            'link': '#TF2/two',
+            'image': './assets/img/tf2TwoPlayers.jpg'
+        }]
+    }, {
+        'title': 'Dota2',
+        'description': 'Dota2 fortress 2 is fun',
+        'link': '#DOTA2',
+        'categories': [{
+            'title': 'Full Team',
+            'link': '#TF2/fullTeam',
+            'image': './assets/img/tf2FullTeam.jpg'
+        }, {
+            'title': 'Party',
+            'link': '#TF2/party',
+            'image': './assets/img/tf2Party.jpg'
+        }, {
+            'title': 'Two Players',
+            'link': '#TF2/two',
+            'image': './assets/img/tf2TwoPlayers.jpg'
+        }]
+    }, {
+        'title': 'CSGO',
+        'description': 'go here: http://www.stratroulette.com/',
+        'link': 'http://www.stratroulette.com/',
+        'categories': [{}]
+    }];
+});

e15e3a6ea0777457474162dde3707d74366fd867
Taylor Premo <tain.premo@gmail.com>
2015-06-30 12:06:10 -0500

    fixed includes

--- app/components/Home/homeController.js
+++ app/components/Home/homeController.js
@@ -1,4 +1,15 @@
-stupidStratGenApp.controller('homeController', function($scope) {
-
-    $scope.message = 'Everyone come and see how good I look!';
+stupidStratGenApp.controller('gameListController', function($scope) {
+    $scope.games = [{
+        'name': 'Team Fortress 2',
+        'description': 'team fortress 2 is fun',
+        'link': '#TF2'
+    }, {
+        'name': 'Dota2',
+        'description': 'Dota2 fortress 2 is fun',
+        'link': '#DOTA2'
+    }, {
+        'name': 'CSGO',
+        'description': 'go here: http://www.stratroulette.com/',
+        'link': 'http://www.stratroulette.com/'
+    }];
 });
